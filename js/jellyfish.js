7b7359aed079436b9ca2ae3edde703ef4acec006
AndresLB0 <86123811+AndresLB0@users.noreply.github.com>
2022-03-05 13:10:56 -0600

    Update jellyfish.js

--- js/jellyfish.js
+++ js/jellyfish.js
@@ -1,1 +1,10 @@
-  M.AutoInit();
+document.addEventListener('DOMContentLoaded', function() {
+  //inicializa el modal
+   var instances = M.Modal.init(document.querySelectorAll('.modal'));
+   //inicializa el menu para moviles
+  var instances = M.Sidenav.init(document.querySelectorAll('.sidenav'));
+  //inicializa las pstañas del modal
+var instances = M.Tabs.init(document.querySelector('.tabs',{
+ swipeable:true,
+	}));
+});
