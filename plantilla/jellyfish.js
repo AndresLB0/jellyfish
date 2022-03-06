a7ec7331506efc4ca54ac782966d6122339912a6
AndresLB0 <86123811+AndresLB0@users.noreply.github.com>
2022-03-06 00:05:31 -0600

    Update jellyfish.js

--- plantilla/jellyfish.js
+++ plantilla/jellyfish.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var instances = M.Parallax.init(document.querySelectorAll('.parallax'));
   //inicializa el modal
    var instances = M.Modal.init(document.querySelectorAll('.modal'));
    //inicializa el menu para moviles
