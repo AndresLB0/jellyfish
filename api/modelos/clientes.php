853ccee819f174e3f9e952f071f5a3d9dcacaaab
Andres Lara <86123811+AndresLB0@users.noreply.github.com>
2022-05-31 14:02:29 -0600

    php de clientes no se si funciona

---
+++ api/modelos/clientes.php
@@ -0,0 +1,208 @@
+<?php
+/*
+*	Clase para manejar la tabla cliente de la base de datos.
+*   Es clase hija de Validator.
+*/
+class cliente extends Validator
+{
+    // Declaración de atributos (propiedades).
+    private $id = null;
+    private $nombres = null;
+    private $apellidos = null;
+    private $correo = null;
+    private $telefono = null;
+    private $clave = null;
+
+    /*
+    *   Métodos para validar y asignar valores de los atributos.
+    */
+    public function setId($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setNombres($value)
+    {
+        if ($this->validateAlphabetic($value, 1, 50)) {
+            $this->nombres = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setApellidos($value)
+    {
+        if ($this->validateAlphabetic($value, 1, 50)) {
+            $this->apellidos = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setCorreo($value)
+    {
+        if ($this->validateEmail($value)) {
+            $this->correo = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setTelefono($value)
+    {
+        if ($this->validatePhone($value)) {
+            $this->telefono = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function setClave($value)
+    {
+        if ($this->validatePassword($value)) {
+            $this->clave = password_hash($value, PASSWORD_DEFAULT);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /*
+    *   Métodos para obtener valores de los atributos.
+    */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNombres()
+    {
+        return $this->nombres;
+    }
+
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    public function getClave()
+    {
+        return $this->clave;
+    }
+
+    /*
+    *   Métodos para gestionar la cuenta del cliente.
+    */
+    public function checkUser($correo)
+    {
+        $sql = 'SELECT id_cliente FROM cliente WHERE correo = ?';
+        $params = array($correo);
+        if ($data = Database::getRow($sql, $params)) {
+            $this->id = $data['id_cliente'];
+            $this->correo = $correo;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function checkPassword($password)
+    {
+        $sql = 'SELECT clave FROM cliente WHERE id_cliente = ?';
+        $params = array($this->id);
+        $data = Database::getRow($sql, $params);
+        if (password_verify($password, $data['clave_cliente'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function changePassword()
+    {
+        $sql = 'UPDATE cliente SET clave = ? WHERE id_cliente = ?';
+        $params = array($this->clave, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function editProfile()
+    {
+        $sql = 'UPDATE cliente
+                SET nombre = ?, apellido = ?, correo= ?, telefono = ?
+                WHERE id_cliente = ?';
+        $params = array($this->nombres, $this->apellidos, $this->correo, $this->telefono, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+    /*
+    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
+    */
+    public function searchRows($value)
+    {
+        $sql = 'SELECT id_cliente, nombre, apellido, correo, telefono
+                FROM cliente
+                WHERE apellido ILIKE ? OR nombre ILIKE ? OR correo ILIKE ?
+                ORDER BY apellido';
+        $params = array("%$value%", "%$value%", "%$value%");
+        return Database::getRows($sql, $params);
+    }
+
+    public function createRow()
+    {
+        $sql = 'INSERT INTO cliente(nombre, apellido, correo, telefono, clave)
+                VALUES(?, ?, ?, ?, ?)';
+        $params = array($this->nombres, $this->apellidos, $this->correo, $this->telefono, $this->clave);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function readAll()
+    {
+        $sql = 'SELECT id_cliente, nombre, apellido, correo
+                FROM cliente
+                ORDER BY apellido';
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT id_cliente, nombre, apellido, correo, telefono
+                FROM cliente
+                WHERE id_cliente = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function updateRow()
+    {
+        $sql = 'UPDATE cliente
+                SET nombre = ?, apellido = ?, telefono = ?
+                WHERE id_cliente = ?';
+        $params = array($this->nombres, $this->apellidos, $this->telefono, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM cliente
+                WHERE id_cliente = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+}
