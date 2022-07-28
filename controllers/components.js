a46cec075af849122facf2c9151d221fba631fc8
AndresLB0 <86123811+AndresLB0@users.noreply.github.com>
2022-07-27 20:06:10 -0600

    ww

--- controllers/components.js
+++ controllers/components.js
@@ -5,7 +5,7 @@
 /*
 *   Constante para establecer la ruta del servidor.
 */
-const SERVER = 'http://localhost/jellyfish2/api/';
+const SERVER = 'http://localhost/jellyfish/api/';
 
 /*
 *   Función para obtener todos los registros disponibles en los mantenimientos de tablas (operación read).
